034597b20bf716f44fa9a8d128d82bd6510a6515
Paolo Chiodi <chiodi84@gmail.com>
2016-09-27 12:28:38 +0200

    extract kubernetes-pod-auth

--- seneca-kubernetes.js
+++ seneca-kubernetes.js
@@ -1,32 +1,9 @@
 
-const fs = require('fs')
-const async = require('async')
+
 const request = require('request')
 const find_ip = require('get-ip-address')
-const caFile = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
-const tokenFile = '/var/run/secrets/kubernetes.io/serviceaccount/token'
+const get_credentials = require('kubernetes-pod-auth')
 
-
-function get_ca_file(next) {
-  fs.readFile(caFile, next)
-}
-
-function get_token_file(next) {
-  fs.readFile(tokenFile, next)
-}
-
-function get_credentials(done) {
-  async.parallel({
-    ca: get_ca_file,
-    token: get_token_file
-  }, function (err, results) {
-    if (err) {
-      return done(err)
-    }
-
-    done(null, results.ca, results.token)
-  })
-}
 
 function get_pods (done) {
   get_credentials(function (err, ca, token) {
